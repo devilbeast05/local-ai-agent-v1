eed6c93b18d94b4cd1b933b27d91c0f137c64f4e
devilbeast05 <lakshay8524132@jmieti.edu.in>
2026-09-19 23:08:01 +0530

    Add interactive local AI agent v1

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from ollama import chat
 
-response = chat(
-model="qwen3:4b",
-messages=[
-    {
-        "role": "user",
-        "content": "Explain what artificial intelligence is."
-    }
-]
-)
-print (response.message.content)
+print("================================")
+print("       LOCAL AI ASSISTANT")
+print("================================")
+print("Model: qwen3:4b")
+print("Type 'exit' to quit.\n")
+
+while True:
+
+    user_input= input("You: ")
+
+    if user_input.lower() == "exit":
+        print("Goodbye!")
+        break
+    response = chat(
+        model="qwen3:4b",
+        messages=[
+            {
+                "role": "user",
+                "content": user_input
+            }
+        ]
+    )
+
+    print("\nAI:", response.message.content)
+    print()
